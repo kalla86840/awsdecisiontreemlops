2674198909c8245ee45902b747af1bbd03f077f0
kalla86840 <122142035+kalla86840@users.noreply.github.com>
2025-09-27 22:23:26 -0700

    Delete aws_mlops_sagemaker directory

--- aws_mlops_sagemaker/utils/aws_utils.py
+++
@@ -1,9 +0,0 @@
-import time
-import uuid
-
-def ts_suffix(prefix: str = "") -> str:
-    return f"{prefix}{int(time.time())}-{uuid.uuid4().hex[:8]}"
-
-def sanitize(name: str) -> str:
-    x = "".join(c if c.isalnum() or c == "-" else "-" for c in name.lower())
-    return x[:63].rstrip("-")
